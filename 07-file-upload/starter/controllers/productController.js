d1512e7999c990ffbe69165f67f6538b81003914
ashu00026 <19341a1207@gmrit.edu.in>
2023-11-24 16:55:22 +0530

    fileUpload

--- 07-file-upload/starter/controllers/productController.js
+++ 07-file-upload/starter/controllers/productController.js
@@ -1,15 +1,17 @@
 const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 
+
 const createProduct = async (req, res) => {
   // console.log(req.body);
+  
   const product = await Product.create(req.body);
   res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-  console.log("Getting All Products");
-  res.send("getting all prodocuts");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = {
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,9 +1,23 @@
 const path = require("path");
 const {StatusCodes}=require("http-status-codes")
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
-  // console.log(req.files);
+  console.log(req.files);
+  if(!req.files){
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
   const image = req.files.image;
+
+
+  if(!image.mimetype.startsWith('image')){
+    throw new CustomError.BadRequestError('please upload an image');
+  }
+  const maxSize=1000*1024;
+  if(image.size>maxSize){
+    throw new CustomError.BadRequestError('please upload an image of size < 1Mb');
+
+  }
   // console.log("path:  "+path);
   // console.log("dir:  "+__dirname);
   const imagePath = path.join(
